2e39d37a6e6c52f2763113d79eda2f9bd9a7cdb1
carlosresendeP <carlosresende.paula@gmail.com>
2025-09-21 21:02:29 -0300

    refactor: Remove código desnecessário e otimiza a configuração do Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
 })
